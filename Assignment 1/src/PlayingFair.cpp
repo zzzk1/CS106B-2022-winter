894b5939de746acc5c482cccfb4a99e9aa5000d3
zzzk1 <madzhou1@gmail.com>
2024-11-27 14:33:17 +0800

    Assignment1 part 3

--- Assignment 1/src/PlayingFair.cpp
+++ Assignment 1/src/PlayingFair.cpp
@@ -7,22 +7,40 @@
 #include "error.h"
 using namespace std;
 
+string aSequenceOfOrder(int n);
+string bSequenceOfOrder(int n);
+
 string aSequenceOfOrder(int n) {
-    /* TODO: Delete this line and the next two lines, then implement this function. */
-    (void) n;
-    return "";
+    if (n < 0) {
+        error("n must be positive number");
+    }
+
+    if (n == 0) {
+        return "A";
+    }
+
+    if (n > 20) {
+        error("n is too large, program will crash.");
+    }
+
+    return aSequenceOfOrder(n - 1) + bSequenceOfOrder(n - 1);
 }
 
 string bSequenceOfOrder(int n) {
-    /* TODO: Delete this line and the next two lines, then implement this function. */
-    (void) n;
-    return "";
+    if (n < 0) {
+        error("n must be positive number");
+    }
+
+    if (n > 20) {
+        error("n is too large, program will crash.");
+    }
+
+    if (n == 0) {
+        return "B";
+    }
+
+    return bSequenceOfOrder(n - 1) + aSequenceOfOrder(n - 1);
 }
-
-
-
-
-
 
 /* * * * * * Provided Test Cases * * * * * */
 
@@ -62,21 +80,10 @@ PROVIDED_TEST("Triggers error on negative inputs.") {
     EXPECT_ERROR(bSequenceOfOrder(-137));
 }
 
-/* TODO: You will need to add your own tests into this suite of test cases. Think about the sorts
- * of inputs we tested here, and, importantly, what sorts of inputs we *didn't* test here. Some
- * general rules of testing:
- *
- *    1. Try extreme cases. What are some very large cases to check? What are some very small cases?
- *
- *    2. Be diverse. There are a lot of possible inputs out there. Make sure you have tests that account
- *       for cases that aren't just variations of one another.
- *
- *    3. Be sneaky. Don't just try standard inputs. Try weird ones that you wouldn't expect anyone to
- *       actually enter, but which are still perfectly legal.
- *
- * Happy testing!
- */
-
+PROVIDED_TEST("Own test.") {
+   EXPECT_ERROR(aSequenceOfOrder(INT_MAX));
+   EXPECT_ERROR(aSequenceOfOrder(INT_MIN));
+}
 
 
 
