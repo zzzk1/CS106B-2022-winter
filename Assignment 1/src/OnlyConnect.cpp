f6870bdf3e9caeb808fb826a73a1956632671b63
zzzk1 <madzhou1@gmail.com>
2024-11-27 14:04:35 +0800

    Assignment1 part 2

--- Assignment 1/src/OnlyConnect.cpp
+++ Assignment 1/src/OnlyConnect.cpp
@@ -8,19 +8,61 @@
 #include "GUI/SimpleTest.h"
 using namespace std;
 
+bool shouldSkip(char ch);
+
+string removeVowelLetter(string str, int index, int len);
+
 string onlyConnectize(string phrase) {
-    /* TODO: The next few lines just exist to make sure you don't get compiler warning messages
-     * when this function isn't implemented. Delete these lines, then implement this function.
-     */
-    (void) phrase;
-    return "";
+    return removeVowelLetter(phrase, 0, phrase.length());
 }
 
+bool shouldSkip(char ch) {
+    if (!isalpha(ch)) {
+        return true;
+    }
 
+    ch = toupper(ch);
+    if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'Y') {
+        return true;
+    }
 
+    return false;
+}
 
+string removeVowelLetter(string str, int index, int len) {
+    if (index >= len) {
+        return "";
+    }
+    string result = removeVowelLetter(str, index + 1, len);
 
+    char ch = toupper(str[index]);
+    if (!shouldSkip(ch)) {
+        return ch + result;
+    }
 
+    return result;
+}
+
+/*
+ * replace locally without any external space cost.
+ * Time: O(n) Space O(1)
+ */
+// string onlyConnectize(string phrase) {
+//     int low = 0;
+//     int fast = 0;
+//     for (; fast < phrase.length(); ) {
+//         char ch = phrase[fast];
+//         if (shouldSkip(ch)) {
+//             fast++;
+//         } else {
+//             ch = toupper(ch);
+//             phrase[low] = ch;
+//             low++;
+//             fast++;
+//         }
+//     }
+//     return phrase.substr(0, low);
+// }
 /* * * * * * Provided Test Cases * * * * * */
 
 PROVIDED_TEST("Converts lower-case to upper-case.") {
@@ -39,22 +81,15 @@ PROVIDED_TEST("Handles single-character inputs.") {
     EXPECT_EQUAL(onlyConnectize("Q"), "Q");
 }
 
-/* TODO: You will need to add your own tests into this suite of test cases. Think about the sorts
- * of inputs we tested here, and, importantly, what sorts of inputs we *didn't* test here. Some
- * general rules of testing:
- *
- *    1. Try extreme cases. What are some very large cases to check? What are some very small cases?
- *
- *    2. Be diverse. There are a lot of possible inputs out there. Make sure you have tests that account
- *       for cases that aren't just variations of one another.
- *
- *    3. Be sneaky. Don't just try standard inputs. Try weird ones that you wouldn't expect anyone to
- *       actually enter, but which are still perfectly legal.
- *
- * Happy testing!
- */
-
-
+PROVIDED_TEST("Own test.") {
+    EXPECT_EQUAL(onlyConnectize(""), "");
+    EXPECT_EQUAL(onlyConnectize("AAAAAAAAAAAA"), "");
+    EXPECT_EQUAL(onlyConnectize("QQQQQQQQQQQ"), "QQQQQQQQQQQ");
+    EXPECT_EQUAL(onlyConnectize("QAQBQQCQQDQ"), "QQBQQCQQDQ");
+    EXPECT_EQUAL(onlyConnectize("中文"), "");
+    EXPECT_EQUAL(onlyConnectize("😅"), "");
+    EXPECT_EQUAL(onlyConnectize("\n"), "");
+}
 
 
 
